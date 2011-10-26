519c59e32fdcfde0ff703c7cca62b775c7459b93
גאבור סבו - Gábor Szabó <gabor@pti.co.il>
2011-10-26 09:51:11 +0000

    Import of SZABGAB/Dwimmer-0.20 from CPAN.
    gitpan-cpan-distribution: Dwimmer
    gitpan-cpan-version:      0.20
    gitpan-cpan-path:         SZABGAB/Dwimmer-0.20.tar.gz
    gitpan-cpan-author:       SZABGAB
    gitpan-cpan-maturity:     released

---
+++ SETUP.bat
@@ -0,0 +1,1 @@
+perl -Ilib script/dwimmer_setup.pl -email szabgab@gmail.com --password dwimmer --root . --dbonly
---
+++ t/lib/Dwimmer/Test.pm
@@ -0,0 +1,96 @@
+package t::lib::Dwimmer::Test;
+use strict;
+use warnings;
+
+use base 'Exporter';
+
+our @EXPORT = qw(start stop $admin_mail @users read_file);
+
+#use File::Basename qw(dirname);
+
+use File::Spec;
+use File::Temp qw(tempdir);
+use File::Copy qw(copy);
+
+my $process;
+
+sub start {
+    my ($password) = @_;
+    #return if $^O !~ /win32/i;    # this test is for windows only now
+
+    my $dir = tempdir( CLEANUP => 1 );
+
+    # print STDERR "# $dir\n";
+
+    $ENV{DWIMMER_TEST} = 1;
+    $ENV{DWIMMER_PORT} = 3001;
+    $ENV{DWIMMER_MAIL} = File::Spec->catfile( $dir, 'mail.txt' );
+
+    our $admin_mail = 'test@dwimmer.org';
+
+    our @users = (
+        {   uname    => 'tester',
+            fname    => 'foo',
+            lname    => 'bar',
+            email    => 'test@dwimmer.org',
+            password => 'dwimmer',
+        },
+    );
+
+    my $root = File::Spec->catdir( $dir, 'dwimmer' );
+    system
+        "$^X -Ilib script/dwimmer_setup.pl --root $root --email $admin_mail --password $password";
+
+    mkdir "$root/polls";
+    copy("t/files/testing-polls.json", "$root/polls");
+
+
+    if ( $^O =~ /win32/i ) {
+        require Win32::Process;
+
+        #import Win32::Process;
+
+        Win32::Process::Create( $process, $^X,
+            "perl -Ilib -It\\lib $root\\bin\\app.pl",
+            0, Win32::Process::NORMAL_PRIORITY_CLASS(), "." )
+            || die ErrorReport();
+    } else {
+	    $process = fork();
+
+        die "Could not fork() while running on $^O" if not defined $process;
+
+        if ($process) { # parent
+            sleep 1;
+            return $process;
+        }
+
+        my $cmd = "$^X -Ilib -It/lib $root/bin/app.pl";
+        exec $cmd;
+    }
+
+    return 1;
+}
+
+sub stop {
+    return if not $process;
+    if ( $^O =~ /win32/i ) {
+        $process->Kill(0);
+    } else {
+        kill 9, $process;
+    }
+}
+
+END {
+    stop();
+}
+
+sub read_file {
+    my $file = shift;
+    open my $fh, '<', $file or die "Could not open '$file' $!";
+    local $/ = undef;
+    my $cont = <$fh>;
+    close $fh;
+    return $cont;
+}
+
+1;
